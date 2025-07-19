4cfb72312394f53e0e54ac98fa4f9bc5d44c272c
bon <bonujumanova@gmail.com>
2025-07-19 11:42:50 +0700

    Было добавлено решение задачи "Угадай число"

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 
 
 * [352. Камни и украшения](./coderun/task_352.py) | [Условие задачи](https://coderun.yandex.ru/problem/rocks-and-jewels/description?compiler=python)
+
+
+
+## guess the number
+
+* [1. Угадай число: Число в памяти](./guess_the_number/memory.py) | [Условие задачи](https://t.me/ne_bon_ju/3)
---
+++ guess_the_number/memory.py
@@ -0,0 +1,25 @@
+import random
+
+hidden_number = random.randint(1, 100)
+
+number_of_attempts: int = 10
+flag = False
+i = 1
+
+while i < 11:
+    try:
+        user_num: int = int(input("Введите число: "))
+    except:
+         print("Введите числовое значение!")
+    else:
+        if user_num < hidden_number:
+            print("Загаданное число больше!")
+        elif user_num > hidden_number:
+            print("Загаданное число меньше")
+        else:
+            print(f"Поздравляем! Вы угадали число за {i} попыток!")
+            break
+        i += 1
+
+else:
+    print(f"К сожалению, вы не угадали число. Загаданное число было {hidden_number}.")
