89595c1a999e7a80ae61db14a6e1dbb621617608
bon <bonujumanova@gmail.com>
2025-07-31 13:29:46 +0700

    Было добавлено решение задачи "Загадай число"

--- guess_the_number/in_your_mind.py
+++ guess_the_number/in_your_mind.py
@@ -1,4 +1,3 @@
-from random import randint
 from math import log
 
 print("Загадайте число в уме, а программа попробует угадать!")
@@ -20,15 +19,15 @@ while True:
             start_num_1, end_num_1 = min(number_1, number_2), max(number_1, number_2)
             break
 
-
 nums = range(start_num_1, end_num_1)
 number_of_attempts: int = int(log(len(nums), 2)) + 1
-
 i = 0
 
-while i != 11:
+while i != number_of_attempts :
+
     program_answer_1 = (start_num_1 + end_num_1) // 2
     print(program_answer_1)
+    i += 1
     user_input: str = input()
     if user_input == "больше":
         start_num_1 = program_answer_1 + 1
@@ -40,5 +39,3 @@ while i != 11:
     if program_answer_1 <= number_1 or program_answer_1 >= number_2:
         print("Ошибка! Похоже, вы дали противоречивые ответы.")
         break
-
-    i += 1
