5e634cf9b8ce0bd70d287f21b42dc9f5f2ae3bf3
bon <bonujumanova@gmail.com>
2025-07-31 13:16:11 +0700

    Было добавлено решение задачи "Загадай число"

--- guess_the_number/in_your_mind.py
+++ guess_the_number/in_your_mind.py
@@ -24,35 +24,21 @@ while True:
 nums = range(start_num_1, end_num_1)
 number_of_attempts: int = int(log(len(nums), 2)) + 1
 
-i: int = 1
-
-program_answer = randint(start_num_1, end_num_1)
-print(program_answer)
+i = 0
 
 while i != 11:
-
-    try:
-        user_input: str = input("Загаданное число ")
-        if program_answer == start_num_1 or program_answer == end_num_1:
-            raise ValueError()
-
-    except ValueError:
+    program_answer_1 = (start_num_1 + end_num_1) // 2
+    print(program_answer_1)
+    user_input: str = input()
+    if user_input == "больше":
+        start_num_1 = program_answer_1 + 1
+    elif user_input == "меньше":
+        end_num_1 = program_answer_1 - 1
+    elif user_input =="угадал":
+        print(f"Я угадал! Загаданное число {program_answer_1}. Количество попыток: {i}")
+        break
+    if program_answer_1 <= number_1 or program_answer_1 >= number_2:
         print("Ошибка! Похоже, вы дали противоречивые ответы.")
         break
-    else:
 
-        if user_input == "больше": # and program_answer != start_num_1:
-            start_num = program_answer
-            program_answer = randint(start_num, end_num_1)
-        elif user_input == "меньше": # and program_answer != end_num_1:
-            end_num = program_answer
-            program_answer = randint(start_num_1, end_num)
-        elif user_input == "угадал":
-            print(f"Я угадал! Загаданное число {program_answer}. Количество попыток: {i}")
-            break
-
-        i += 1
-        print(program_answer)
-
-else:
-    print("К сожалению, вы не справились!")
+    i += 1
