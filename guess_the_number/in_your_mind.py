ec492d12df5d116a4a61273a82d1eabe244018ec
Shoxrux Yuldashov <deagleshox@mail.ru>
2025-08-13 07:02:51 +0100

    Разбор ДЗ
    - Улучшено решение:
        - Угадай число: Число в памяти
        - Угадай число: Число в уме (доработать)

--- guess_the_number/in_your_mind.py
+++ guess_the_number/in_your_mind.py
@@ -1,41 +1,70 @@
 from math import log
 
-print("Загадайте число в уме, а программа попробует угадать!")
-print()
-
+print("Загадайте число в уме, а программа попробует угадать!\n")
 while True:
-    try:
-        number_1:int = int(input("Введите начало диапазона загаданного числа"))
-        number_2: int = int(input("Введите конец диапазона загаданного числа"))
+    start, end = input(
+        "Введите начало и конец диапазона загаданного числа > "
+    ).split()
 
-    except ValueError:
+    if not (start.isdigit() and end.isdigit()):
         print("Введите целое натуральное число!!!")
+        continue
 
-    else:
-        if number_1 == number_2:
-            print("Числа не должны быть равны!")
+    if start == end:
+        print("Числа не должны быть равны!")
+        continue
 
-        else:
-            start_num_1, end_num_1 = min(number_1, number_2), max(number_1, number_2)
-            break
+    start, end = int(start), int(end)
+    start_num_1, end_num_1 = min(start, end), max(start, end)
+    break
 
-nums = range(start_num_1, end_num_1)
+nums: range = range(start_num_1, end_num_1)
+answers: tuple[str, str, str] = ("больше", "меньше", "угадал")
 number_of_attempts: int = int(log(len(nums), 2)) + 1
-i = 0
-
-while i != number_of_attempts :
-
+i: int = 0
+flag: bool = True
+while i < number_of_attempts:
     program_answer_1 = (start_num_1 + end_num_1) // 2
-    print(program_answer_1)
-    i += 1
+    if flag:
+        print(program_answer_1)
+        i += 1
+
     user_input: str = input()
+    if user_input not in answers:
+        print(f"Возможные варианты ответов: {answers}")
+        flag = False
+        continue
+
+    flag = True
+    if user_input == "угадал":
+        print(
+            f"Я угадал! Загаданное число {program_answer_1}. "
+            f"Количество попыток: {i}"
+        )
+        break
+
     if user_input == "больше":
         start_num_1 = program_answer_1 + 1
     elif user_input == "меньше":
         end_num_1 = program_answer_1 - 1
-    elif user_input =="угадал":
-        print(f"Я угадал! Загаданное число {program_answer_1}. Количество попыток: {i}")
-        break
-    if program_answer_1 <= number_1 or program_answer_1 >= number_2:
-        print("Ошибка! Похоже, вы дали противоречивые ответы.")
-        break
+else:
+    # if program_answer_1 not in nums:
+    print("Ошибка! Похоже, вы дали противоречивые ответы.")
+    # break
+
+# TODO: После вывода 1, следующим должен быть ответ:
+#  'Ошибка! Похоже, вы дали противоречивые ответы.'
+"""
+Введите начало и конец диапазона загаданного числа > 1 30
+15
+меньше
+7
+меньше
+3
+меньше
+1
+меньше
+0
+меньше
+Ошибка! Похоже, вы дали противоречивые ответы.
+"""
--- guess_the_number/memory.py
+++ guess_the_number/memory.py
@@ -1,24 +1,27 @@
 import random
 
-hidden_number = random.randint(1, 100)
+hidden_number: int = random.randint(1, 100)
 
 number_of_attempts: int = 10
-i = 1
-
-while i < 11:
+i: int = 1
+while i < number_of_attempts:
     try:
         user_num: int = int(input("Введите число: "))
     except ValueError:
-         print("Введите числовое значение!")
+        print("Введите числовое значение!")
     else:
+        if user_num == hidden_number:
+            print(f"Поздравляем! Вы угадали число за {i} попыток!")
+            break
+
         if user_num < hidden_number:
             print("Загаданное число больше!")
         elif user_num > hidden_number:
             print("Загаданное число меньше")
-        else:
-            print(f"Поздравляем! Вы угадали число за {i} попыток!")
-            break
-        i += 1
 
+        i += 1
 else:
-    print(f"К сожалению, вы не угадали число. Загаданное число было {hidden_number}.")
+    print(
+        "К сожалению, вы не угадали число. "
+        f"Загаданное число было {hidden_number}."
+    )
