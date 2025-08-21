b2b9b2fc6b9212ab82f30144747b2e15c9614e61
bon <bonujumanova@gmail.com>
2025-08-21 11:33:02 +0700

    Было добавлено решение задачи Парольнадзор с помощью функций

--- functions/f_pass_control.py
+++ functions/f_pass_control.py
@@ -7,43 +7,42 @@ error_message = [
     "Пароль должен заканчиваться только латинской буквой или цифрой",
     "Минимальная длина пароля — 12 символов, максимальная — 32 символа"
 ]
-
 print(*error_message, sep="\n", end="\n\n")
-
-min_length: int = 12
-max_length: int = 32
-underscore: str = "_"
-letters: str = string.ascii_letters
 
 
 def find_digit(password):
     for digit in string.digits:
-        if digit in user_password:
+        if digit in password:
             return True
-    return "Пароль должен состоять только из латинских букв, цифр и символа нижнего подчёркивания ( _ )"
+    print("Пароль должен состоять только из латинских букв, цифр и символа нижнего подчёркивания ( _ )")
+    return False
 
 
 def find_upper_letter(password):
-    if user_password[0] not in string.ascii_uppercase:
-        return "Пароль должен начинаться с заглавной буквы"
+    if password[0] not in string.ascii_uppercase:
+        print("Пароль должен начинаться с заглавной буквы")
+        return False
     return True
 
 
-def find_underscore(password):
-    if underscore not in user_password:
-        return "Пароль должен содержать символ нижнего подчеркивания"
+def has_underscore(password):
+    if "_" not in password:
+        print("Пароль должен содержать символ нижнего подчеркивания")
+        return False
     return True
 
 
-def find_length(password):
-    if not(min_length <= len(user_password) <= max_length):
-        return "Длина пароля не соответствует требованиям"
+def find_length(password, min_length, max_length):
+    if not(min_length <= len(password) <= max_length):
+        print("Длина пароля не соответствует требованиям")
+        return False
     return True
 
 
 def check_last_sym(password):
-    if password[-1] not in (letters + string.digits):
-        return "Пароль должен заканчиваться только латинской буквой или цифрой"
+    if password[-1] not in (string.ascii_letters + string.digits):
+        print("Пароль должен заканчиваться только латинской буквой или цифрой")
+        return False
     return True
 
 
@@ -53,26 +52,32 @@ def get_password():
 
 
 def main():
-    global user_password
+
+    min_length: int = 12
+    max_length: int = 32
 
     user_password = get_password()
     a = find_digit(user_password)
     b = find_upper_letter(user_password)
-    c = find_length(user_password)
-    d = find_underscore(user_password)
+    c = find_length(user_password, min_length, max_length)
+    d = has_underscore(user_password)
     e = check_last_sym(user_password)
 
-    if a == b == c == d == e:
+
+    if all([a, b, c, d, e]):
         print("Пароль принят!")
         with open("valid_passwords.txt", "a", encoding="UTF-8") as f:
             f.write(user_password + "\n")
-    else:
-        for result in [a, b, c, d, e]:
-            if result != True:
-                print(result)
+        return True
+    return False
 
 
-        main()
+flag: bool = False
+while True:
 
 
-main()
+    result = main()
+
+
+    if result:
+        break
