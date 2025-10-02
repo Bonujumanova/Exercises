b18f962ac172598f300b92495800f019f6a7d3f5
bon <bonujumanova@gmail.com>
2025-10-02 16:11:47 +0700

    Было добавлено недорешение задачи tic tac toe

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,3 +1,7 @@
+from colorama import init, Fore, Back, Style
+init()
+
+
 data: list[list[int | str]] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 for col in range(len(data)):
     for row in range(len(data[col])):
@@ -10,12 +14,14 @@ for col in range(len(data)):
 
 
 def show_table(char: str, player_choice: int) -> list:
+    # if char == "O":
+    #     color = Fore.YELLOW
+    # else:
+    #     color = Fore.GREEN
     for col in range(len(data)):
         for row in range(len(data[col])):
-
             if data[col][row] == player_choice:
                 data[col][row] = char
-
             if row != 2:
                 print(data[col][row], "|", end=" ")
             else:
@@ -35,44 +41,94 @@ def check_winner(changed_data: list[list]) -> bool:
             return True
         for row in range(col, col + 1):
             if changed_data[0][row] ==  changed_data[1][row] == changed_data[2][row]:
-                print(f"Пебедил игрок: {players.get(changed_data[col][row])}, '2'")
+                print(f"Победил игрок: {players.get(changed_data[col][row])}, '2'")
                 return True
 
         if changed_data[col][0] == changed_data[col][1] == changed_data[col][2]:
-            print(f"Пебедил игрок: {players.get(changed_data[col][1])} '3'")
+            print(f"Победил игрок: {players.get(changed_data[col][1])} '3'")
             return True
         elif changed_data[col][2] == changed_data[col][1] == changed_data[col][0]:
-            print(f"Пебедил игрок: {players.get(changed_data[col][1])} '4'")
+            print(f"Победил игрок: {players.get(changed_data[col][1])} '4'")
             return True
 
+    if all(all((str(j).isalpha() for j in i)) for i in data):
+        print("Победителя нет! Постарайтесь выиграть в следующий раз!!!")
+        return True
     return False
 
-count = 0
-second = True
+
+
+def get_players_input(first: bool, second: bool, entered_numbers, players_name):
+
+    symbol: str = ""
+    player: int = 0
+
+    if second:
+        while True:
+            try:
+                player: int = int(input(f"Ходит игрок 1 {players_name["X"]}: "))
+                if 1 <= player <= 9 and player not in entered_numbers:
+                    second = False
+                    first = True
+                    symbol = "X"
+                    entered_numbers.append(player)
+                    break
+                elif player in entered_numbers:
+                    print("Это окно уже занято, выберите другое!")
+                else:
+                    raise ValueError("Введите число от '1' до '9'")
+            except ValueError:
+                print("Введите число от '1' до '9'")
+
+
+    else:
+        while True:
+            try:
+                player: int = int(input(f"Ходит игрок 2 {players_name["O"]}: "))
+                if 1 <= player <= 9 and player not in entered_numbers:
+                    first = False
+                    second = True
+                    symbol = "O"
+                    entered_numbers.append(player)
+                    break
+                elif player in entered_numbers:
+                    print("Это окно уже занято, выберите другое!")
+                else:
+                    raise ValueError("Введите число от '1' до '9'")
+            except ValueError:
+                print("Введите число от '1' до '9'")
+
+    return player, symbol, first, second
+
+
+
+
 def main():
-    global count
-    global second
-    players: dict = {"X": "player_1_name", "O": "player_1_name"}
-
-
+    first_player_name: str = input("Игрок №1, введите Ваше имя: ")
+    second_player_name: str = input("Игрок №2, введите Ваше имя: ")
+    players_name: dict = {"X": first_player_name, "O": second_player_name}
+    first = False
+    second = True
+    entered_numbers: list[int] = []
     while True:
 
-        if second:
-            player: int = int(input(f"Ходит игрок 1 {players["X"]}: "))
-            second = False
-            symbol = "O"
-
-        else:
-            player: int = int(input(f"Ходит игрок 2 {players["O"]}: "))
-            second = True
-            symbol = "X"
-
-
-        table = show_table(symbol, player)
+        entered_number, symbol, *bools = get_players_input(first, second, entered_numbers, players_name)
+        table = show_table(symbol, entered_number)
         res = check_winner(table)
         if res:
             break
+        first = bools[0]
+        second = bools[1]
+    return res
 
 
-main()
+result = main()
+
+if result:
+    player_choice: str = input("Хотите сыграть еще раз?(y/n) ")
+    if player_choice == "y":
+
+
+        main()
+
 
