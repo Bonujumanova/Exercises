26b6bbc03b30da816b6620fd8427ed25bc793efd
bon <bonujumanova@gmail.com>
2025-10-14 11:51:19 +0700

    Попытка 2 Было добавлено недорешение задачи tic tac toe

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -2,7 +2,7 @@ from colorama import init, Fore, Back, Style
 init()
 
 
-def show_current_table(data: list, char="", player_choice="") -> list:
+def show_current_table(data: list, char="", player_choice=0) -> list:
     if char == "O":
         color = Fore.YELLOW
     else:
@@ -22,29 +22,30 @@ def show_current_table(data: list, char="", player_choice="") -> list:
     return data
 
 
-def check_winner(changed_data: list[list]):
+def check_winner(changed_data: list[list], count_players_moves: int):
 
     for col in range(len(changed_data)):
         if all(changed_data[col][0] == changed_data[col][row] for row in range(len(changed_data[col]))):
-            return "win", changed_data[col][0]
+            return "win"
 
         for row in range(col, col + 1):
             if changed_data[0][row] ==  changed_data[1][row] == changed_data[2][row]:
-                return "win", changed_data[0][row]
+                return "win"
 
         if changed_data[0][0] == changed_data[1][1] == changed_data[2][2]:
-            return "win", changed_data[1][1]
+            return "win"
 
         elif changed_data[0][2] == changed_data[1][1] == changed_data[2][0]:
-            return True, "win", changed_data[1][1]
+            return "win"
 
-    if all(all((str(j).isalpha() for j in i)) for i in changed_data):
-        return "loss", None
+    # if all(all((str(j).isalpha() for j in i)) for i in changed_data):
+    if count_players_moves == 9:
+        return "loss"
     return False
 
 
 
-def get_players_input(first: bool, second: bool, entered_numbers, players_name):
+def get_players_input(first: bool, second: bool, players_name, table):
     if second:
         current_symbol: str = "X"
         second : bool= False
@@ -56,32 +57,36 @@ def get_players_input(first: bool, second: bool, entered_numbers, players_name):
         second: bool = True
         player_num: int = 2
 
+    cur_result = False
     while True:
         try:
-            player: int = int(input(f"Ходит игрок {player_num} {players_name["X"]}: "))
-            if 1 <= player <= 9 and player not in entered_numbers:
-                entered_numbers.append(player)
-                break
-            elif player in entered_numbers:
-                print("Это окно уже занято, выберите другое!")
+            player: int = int(input(f"Ходит игрок {player_num} {players_name['X']}: "))
+            if 1 <= player <= 9:
+                for row in table:
+                    if player in row:
+                        cur_result = True
+                        break
             else:
                 raise ValueError
+
+            if cur_result:
+                break
+            else:
+                print("Это окно уже занято, выберите другое!")
         except ValueError:
             print("Введите число от '1' до '9'")
-
     return player, current_symbol, first, second
 
 
 
 def main():
-
     winner_text: dict[str, str] = {
         "win": "Победил игрок:",
         "loss": "Победителя нет! Постарайтесь выиграть в следующий раз!!!"
     }
     main_table: list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-    show_current_table(main_table, )
+    show_current_table(main_table)
+    count_players_moves: int = 0
     while True:
         try:
             first_player_name: str = input("Игрок №1, введите Ваше имя: ")
@@ -95,21 +100,19 @@ def main():
             players_name: dict = {"X": first_player_name, "O": second_player_name}
             first = False
             second = True
-            entered_numbers: list[int] = []
             table: list[list[int | str]] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
             while True:
-                entered_number, symbol, *bools = get_players_input(first, second, entered_numbers, players_name)
-                table = show_current_table(table,symbol, entered_number)
-                res = check_winner(table)
-
+                entered_number, cur_symbol, *bools = get_players_input(first, second, players_name, table)
+                count_players_moves += 1
+                table = show_current_table(table,cur_symbol, entered_number)
+                res = check_winner(table, count_players_moves)
                 if res:
-                    winner_name: str = ""
-                    if players_name.get(res[1]):
-                        winner_name: str = players_name.get(res[1])
-                    print(winner_text.get(res[0]), winner_name)
+                    if res == "win":
+                        print(winner_text.get(res), players_name.get(cur_symbol))
+                    else:
+                        print(winner_text.get(res))
                     break
-
                 first = bools[0]
                 second = bools[1]
             return res
@@ -123,6 +126,3 @@ while result:
         result = main()
     else:
         break
-
-
-
